b4f8fbea2523a691884fba4aa838404a36b48e1f
Ben Tatman <ben@tatmans.co.uk>
2014-03-02 20:09:22 +0000

    Working on balancing vectors. Should be simpler now

--- classes.py
+++ classes.py
@@ -1,45 +1,121 @@
+import math
+
 def reduce_to(value, below):
 	while (value > below):
 		value = value - below
 	return value
 
+
+
 class Position:
-	x;
-	y;
+	x = 0;
+	y = 0;
 	def __init__(self, x, y):
 		self.x = x;
 		self.y = y;
 
 	def translate(self, angle, distance):
 		angle = reduce_to(angle, 360)
-		lateral_x = distance * cos(reduce_to(angle, 90))
-		lateral_y = distance * sin(reduce_to(angle, 90))
+		lateral_x = distance * math.cos(reduce_to(angle, 90))
+		lateral_y = distance * math.sin(reduce_to(angle, 90))
 
 		if (angle < 90):
-			# do nothing
-		else if (angle < 180):
+			a = 1
+		elif (angle < 180):
 			# 2nd quadrant, y is negative
 			lateral_y = -(lateral_y)
-		else if (angle < 270):
+		elif (angle < 270):
 			# 3rd, y and x negative
 			lateral_y = -(lateral_y)
 			lateral_x = -(lateral_x)
-		else if (angle < 360):
+		elif (angle < 360):
 			lateral_x = -(lateral_x)
 
 		self.x = self.x + lateral_x
 		self.y = self.y + lateral_y
+	
+	def to_string(self):
+		return "X: "+str(self.x)+"\nY: "+str(self.y)
+		
 
 class Vector:
-	magnitude;
-	direction;
+	magnitude = 0;
+	direction = 0;
 	def __init__(self, magnitude, direction):
 		self.magnitude = magnitude
 		self.direction = direction
 
-	def to_components(self):
-		
+	def to_string(self):
+		return "Mag: "+str(self.magnitude)+"\nAngle: "+str(self.direction)
 
+	def to_components(self):
+		angle = reduce_to(self.direction, 360)
+		angle_l = reduce_to(self.direction, 90)
+		
+		x = 0
+		y = 0
+		
+		if (angle < 0):
+			angle = angle + 360
+		
+		if (angle >= 0 and angle <= 90):
+			x = self.magnitude * math.sin(angle_l)
+			y = self.magnitude * math.cos(angle_l)
+		elif (angle >= 90 and angle <= 180):
+			x = self.magnitude * math.cos(angle_l)
+			y = -(self.magnitude * math.sin(angle_l))
+		elif (angle >= 180 and angle <= 270):
+			x = -(self.magnitude * math.sin(angle_l))
+			y = -(self.magnitude * math.cos(angle_l))
+		elif (angle >= 270):
+			x = -(self.magnitude * math.cos(angle_l))
+			y = self.magnutide * math.sin(angle_l)
+		
+		return Position(x, y)		
+
+def balance_vectors(vectors):
+	total = Position(0, 0)
+	po = Position(0, 0)
+	for i in range(0, len(vectors)):
+		po = vectors[i].to_components()
+		print(po.to_string())
+		total.x += po.x
+		total.y += po.y
+	l = Vector(0, 0)
+	l.magnitude = math.sqrt((total.x*total.x)+(total.y*total.y))
+	l.direction = math.atan2(total.y, total.x)
+	return l
 
 class Particle:
-	position;
+	position = Position(0,0)
+	forces = []
+	tmp_forces = []
+	motion = Vector(0,0)
+	name = ""
+	mass = 0
+	def __init__(self, forces, position, tmp_forces, name, mass, motion):
+		self.position = position
+		self.forces = forces
+		self.tmp_forces = tmp_forces
+		self.name = name
+		self.mass = mass
+		self.motion = motion
+		
+	def to_string(self):
+		stri =  "Name: "+self.name+"\nMass: "+str(self.mass)+"\nPosition: \n"+self.position.to_string()
+		for i in range(0, len(self.forces)):
+			stri += "\nForce:\n"+self.forces[i].to_string()
+		for i in range(0, len(self.tmp_forces)):
+			stri += "\nTMP Force:\n"+self.tmp_forces[i].to_string()
+		return stri
+	
+
+
+vec = Vector(2, (math.pi/4))
+par = Particle([vec], Position(10, 5), [], "Mars", 10, Vector(2, 0))
+#print(par.to_string())
+
+vect = Vector(2, (math.pi*7/4))
+
+l = balance_vectors([vec, vect])
+print(l.to_string())
